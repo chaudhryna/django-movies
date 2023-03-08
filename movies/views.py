90e5a77879b31161c0a98392ee9461d9f5982d14
Nancy Chaudhry <nancych14@gmail.com>
2023-03-08 18:59:36 +0500

    Finished course

--- movies/views.py
+++ movies/views.py
@@ -1,32 +1,35 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.shortcuts import render
+from .models import Movie
 
 def movies(request):
-  context = {
-    'movies': [
-      {
-        'id': 8,
-        'title': 'Jaws',
-        'year': 1975
-      },
-      {
-        'id': 2,
-        'title': 'Cleopatra',
-        'year': 1963
-      },
-      { 
-        'id': 3,
-        'title': 'La Cage aux Folles',
-        'year': 1978
-      },
-      {
-        'id': 4,
-        'title': 'Moonstruck',
-        'year': 1987
-      }
-    ]
-  }
+  movies = Movie.objects.all()
+  context = {'movies': movies}
   return render(request, 'movies/movies.html', context)
 
 def home(request):
   return render
+
+def detail(request, id):
+  movie = Movie.objects.get(pk=id)
+  context = {'movie': movie}
+  return render(request, 'movies/detail.html', context)
+
+def add(request):
+  title = request.POST.get('title')
+  year = request.POST.get('year')
+
+  if title and year:
+    movie = Movie(title=title, year=year)
+    movie.save()
+    return HttpResponseRedirect('/movies')
+
+  return render(request, 'movies/add.html')
+
+def delete(request, id):
+  try:
+    movie = Movie.objects.get(pk=id)
+  except:
+    raise Http404('Movie does not exist.')
+  movie.delete()
+  return HttpResponseRedirect('/movies')
